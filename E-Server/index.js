9924581e68f24788be0ee6a59dc77fac9165a0ee
devprashant <kumarjha183@gmail.com>
2025-05-15 09:53:38 +0530

    initial commit

---
+++ E-Server/index.js
@@ -0,0 +1,50 @@
+// packages
+import path from "path";
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+// Utiles
+import connectDB from "./config/db.js";
+import userRoutes from "./routes/users.routes.js";
+import categoryRoutes from "./routes/categories.routes.js";
+import productRoutes from "./routes/products.routes.js";
+import uploadRoutes from "./routes/uploads.routes.js";
+import orderRoutes from "./routes/orders.routes.js";
+import cors from "cors";
+
+dotenv.config();
+const port = process.env.PORT || 5000;
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(
+    cors({
+        origin: process.env.CLIENT_URL || "http://localhost:5173",
+    })
+);
+  
+
+// app.use("/", (req, res, next) => {
+//   res.send("API is running...");
+// });
+
+app.use("/api/users", userRoutes);
+app.use("/api/category", categoryRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/upload", uploadRoutes);
+app.use("/api/orders", orderRoutes);
+
+app.get("/api/config/paypal", (req, res) => {
+    res.send({ clientId: process.env.PAYPAL_CLIENT_ID });
+});
+
+const __dirname = path.resolve();
+app.use("/uploads", express.static(path.join(__dirname + "/uploads")));
+
+app.listen(port, () => console.log(`Server running on port: ${port}`));
